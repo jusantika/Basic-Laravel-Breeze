a0beaff23de35370444310c2c856e2fe9e71beb7
Jusantika <jusantika@users.noreply.github.com>
2021-03-24 12:33:34 +0800

    Initialized Basic Laravel Breeze

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::view('/', 'home')->name('home');
+Route::view('/about', 'about')->name('about');
+
+Route::middleware(['auth', 'verified'])->group(function () {
+  Route::get('/dashboard', function () {
+      return view('dashboard');
+  })->name('dashboard');
+});
+
+require __DIR__.'/auth.php';
